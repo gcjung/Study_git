fb1c2831d1b4ae3ba4dcce1bdec439c3e053e350
GCJung <gcjung1227@gmail.com>
2022-03-19 22:44:47 +0900

    print 함수 추가

--- git_test/git_test/main.cpp
+++ git_test/git_test/main.cpp
@@ -2,7 +2,18 @@
 
 using namespace std;
 
+void print_second_commit();
+
 int main()
 {
+	
+	print_second_commit();
+
 	cout << "first commit test" << endl;
+}
+
+void print_second_commit()
+{
+	for(int i=0;i<10;i++)
+		cout << "second commit " << endl;
 }
